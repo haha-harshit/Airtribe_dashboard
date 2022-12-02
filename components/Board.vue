980016384d7ee65e1993671d11cc50822c16e43e
Harshit Kumar <bkharshitbk@gmail.com>
2022-12-03 04:18:58 +0530

    (feat): create new list in board

--- components/Board.vue
+++ components/Board.vue
@@ -1,4 +1,5 @@
 <template>
+<div data-app>
     <div class="d-flex flex-row">
         <v-btn depressed @click="dialog=true" class="create-list">
             New list
@@ -15,7 +16,7 @@
                     <v-container>
                     <v-row>
                         <v-col cols="12">
-                        <v-text-field label="To do" v-model="list.title" id="inp" required>
+                        <v-text-field label="Title" v-model="list.title" id="inp" required>
                         </v-text-field>
                         </v-col>
                     </v-row>
@@ -36,39 +37,66 @@
         </v-dialog>
 
     </div>
+</div>
 </template>
 
 <script>
+const board = []
 import { v4 as uuidv4, v4 } from 'uuid'
 export default {
+    
     name: "Board",
     data(){
         return {
+            // board: [{
+            //     list: {
+            //         list_id: '',
+            //         title: '',
+            //         cards: [{
+            //             cards_id: '',
+            //             cards_title: '',
+            //         }]
+            //     }
+            // }],
+            board: {},
+            list_id: '',
             list: {
-                list_id: '',
-                title: '',
-                cards: [{
-                    cards_id: '',
-                    cards_title: '',
-                }]
+                title: ''
             },
+            card: {
+                title: ''
+            },
+            currentCard: {},
+            cardDraggedId: '',
+            cardDraggedListId: '',
             dialog: false,
+            dialogCard: false,
+            dialogEditCard: false,
+            drawer: false,
         }
     },
     methods: {
         async createList(){
+            // console.log(board)
             let that = this
+            that.dialog = false
             const newListInp = document.getElementById("inp").value
             // console.log(newListInp)
-            if(that.list.title !=''){
-                // this.list.list_id = Math.floor((Math.random()*10000) + 1);
-                that.list.list_id = uuidv4()
-                console.log(that.list.list_id)
-                // that.list.title = ''
-                that.list = {},
-                that.list.cards = [],
-                console.log(newListInp);
-                that.dialog = false
+            if( that.list.title != '' ){
+                that.list.id = uuidv4()
+                that.list.cards = []
+                if(that.board.lists){
+                    that.board.lists.push(that.list)
+                }else{
+                    that.board.lists = []
+                    that.board.lists.push(that.list)
+                }
+                that.list = {}
+                // console.log(that.list.cards)
+                localStorage.setItem('board', JSON.stringify(that.board))
+                // console.log(newListInp);
+                // that.dialog = false
+                console.log(that.board)
             }
         },
 
