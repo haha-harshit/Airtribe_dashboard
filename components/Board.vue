aceab23518178986632431ce63228d274259d283
Harshit Kumar <bkharshitbk@gmail.com>
2022-12-04 10:59:13 +0530

    set dialog for each card

--- components/Board.vue
+++ components/Board.vue
@@ -98,8 +98,31 @@
 
             </v-card>
         </v-dialog>
-
     </div>
+
+    <v-dialog v-model="dialogEditCard" persistent max-width="600px">
+        <v-card elevation="0">
+            <v-card-title>
+                <span class="headline">{{ currentCard.title }}</span>
+            </v-card-title>
+            <v-card-text>
+                <v-container>
+                    <v-row>
+                        <v-col cols="12">
+                            <v-text-field label="Edit Title" v-model="currentCard.title" required></v-text-field>
+                        </v-col>
+                    </v-row>
+                </v-container>
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer></v-spacer>
+                <v-btn color="red darken-1" text @click="deleteCard()">Delete</v-btn>
+                <v-btn color="blue darken-1" text @click="dialogEditCard=false">Close</v-btn>
+                <v-btn color="blue darken-1" text @click="updateCard()">Update</v-btn>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
+
     </div>
     </div>
 </div>
@@ -252,6 +275,13 @@ export default {
                 that.listId =''
             }
         },
+        
+
+        editCard(card){
+            this.dialogEditCard = true
+            this.currentCard = card
+        },
+
         async updateBoard(){
             let that = this
             // console.log('here')
